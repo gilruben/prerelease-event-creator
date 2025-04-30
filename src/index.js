8a39de7054635683b2f649e8a28b6d8cbf00884f
Ruben Gil <gil.r419@gmail.com>
2025-04-29 22:57:57 -0400

    Place prerelease event creation code under a cron job

--- src/index.js
+++ src/index.js
@@ -4,21 +4,26 @@ const { storeIds } = require('./storeIds');
 const { getPrereleaseEvents } = require('./utils/wizards');
 const { processPrereleaseEvents } = require('./utils/helpers');
 const { isGuildValid } = require('./utils/discord');
+const cron = require('node-cron');
 
 const { GatewayIntentBits } = Discord;
 const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', async () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-
-  const guildExists = isGuildValid();
-
+  
+  const guildExists = isGuildValid(client);
+  
   if (!guildExists)
     throw new Error('Guild not found');
-
-  const prereleaseEvents = await getPrereleaseEvents(storeIds);
-
-  await processPrereleaseEvents(client, prereleaseEvents);
+  
+  // Checks for prerelease events every third hour (ex: 12pm, 3pm, 6pm, etc)
+  cron.schedule('* */3 * * *', async () => {
+    console.log('CRON-CALLED')
+    const prereleaseEvents = await getPrereleaseEvents(storeIds);
+  
+    await processPrereleaseEvents(client, prereleaseEvents);
+  });
 });
-
+  
 client.login(process.env.CLIENT_TOKEN); //signs the bot in with token
